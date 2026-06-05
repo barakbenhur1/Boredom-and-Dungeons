0a8932046c64d52e14e918578e5f7adb1fa45ebb
barakbenhur1 <78704946+barakbenhur1@users.noreply.github.com>
2026-06-05 04:06:50 +0300

    Add runtime horse feature repair bootstrap

---
+++ Assets/_Project/Scripts/Runtime/BDHorseRuntimeRepair.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace BoredomAndDungeons
+{
+    public static class BDHorseRuntimeRepair
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        private static void InstallAfterSceneLoad()
+        {
+            RepairAllHorses();
+            SceneManager.sceneLoaded -= HandleSceneLoaded;
+            SceneManager.sceneLoaded += HandleSceneLoaded;
+        }
+
+        private static void HandleSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            RepairAllHorses();
+        }
+
+        public static void RepairAllHorses()
+        {
+            BDHorseHealth[] horses = Object.FindObjectsByType<BDHorseHealth>(FindObjectsSortMode.None);
+
+            for (int i = 0; i < horses.Length; i++)
+            {
+                BDHorseHealth horseHealth = horses[i];
+                if (horseHealth == null)
+                    continue;
+
+                GameObject horse = horseHealth.gameObject;
+
+                EnsureComponent<BDHorseWorldStatusIndicator>(horse);
+                EnsureComponent<BDHorseHealAvailabilityIndicator>(horse);
+                EnsureComponent<BDHorseNeedsHealingEffect>(horse);
+                EnsureComponent<BDHorseBuckRepair>(horse);
+
+                if (horse.GetComponent<BDHorseController>() != null)
+                {
+                    EnsureComponent<BDHorseCombatFleeController>(horse);
+                    EnsureComponent<BDHorseReliableFleeMotor>(horse);
+                }
+            }
+        }
+
+        private static T EnsureComponent<T>(GameObject target) where T : Component
+        {
+            T component = target.GetComponent<T>();
+            return component != null ? component : target.AddComponent<T>();
+        }
+    }
+}
