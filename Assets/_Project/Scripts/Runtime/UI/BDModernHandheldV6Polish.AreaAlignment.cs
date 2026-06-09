cd7bb3250cd07437e6e8418fc5efd543eb6ab51f
barakbenhur1 <78704946+barakbenhur1@users.noreply.github.com>
2026-06-09 17:29:26 +0300

    Align pause interaction areas with moved rows

--- Assets/_Project/Scripts/Runtime/UI/BDModernHandheldV6Polish.AreaAlignment.cs
+++ Assets/_Project/Scripts/Runtime/UI/BDModernHandheldV6Polish.AreaAlignment.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 namespace BoredomAndDungeons
 {
     public sealed partial class BDModernHandheldV6Polish
@@ -9,6 +11,36 @@ namespace BoredomAndDungeons
             float width,
             float height)
         {
+            BDModernHandheldControlTarget[] areas =
+                deviceRoot.GetComponentsInChildren<
+                    BDModernHandheldControlTarget>(true);
+
+            for (int areaIndex = 0;
+                 areaIndex < areas.Length;
+                 areaIndex++)
+            {
+                BDModernHandheldControlTarget area = areas[areaIndex];
+                if (area == null ||
+                    area.Action !=
+                    BDModernHandheldControlTarget.ControlAction.ScreenItem ||
+                    area.Index != index)
+                {
+                    continue;
+                }
+
+                area.transform.localPosition = new Vector3(
+                    x / CanvasSize.x * ScreenWidth,
+                    ScreenCenterY +
+                    y / CanvasSize.y * ScreenHeight,
+                    -0.27f
+                );
+                area.transform.localScale = new Vector3(
+                    width / CanvasSize.x * ScreenWidth,
+                    height / CanvasSize.y * ScreenHeight,
+                    0.08f
+                );
+                return;
+            }
         }
     }
 }
